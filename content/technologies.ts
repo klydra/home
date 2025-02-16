8bcd7dd549e458b28b13e7928ec3ec91062f86a4
Jan Klinge <jan@klydra.dev>
2025-02-16 14:31:19 +0100

    Add tech

--- content/technologies.ts
+++ content/technologies.ts
@@ -1,16 +1,286 @@
 type ContentTechnology = {
   id: string;
   title: string;
+  color: string;
+  link: string;
 };
 
 export const contentTechnologies = [
   {
-    id: "react",
-    title: "React",
+    id: "turso",
+    title: "Turso",
+    color: "#4ff8d2",
+    link: "https://turso.tech",
+  },
+  {
+    id: "oauth2",
+    title: "OAuth 2",
+    color: "#808080",
+    link: "https://oauth.net/2/",
   },
   {
     id: "unreal-engine",
     title: "Unreal Engine",
+    color: "#808080",
+    link: "https://www.unrealengine.com",
+  },
+  {
+    id: "unity",
+    title: "Unity",
+    color: "#808080",
+    link: "https://www.unity.com",
+  },
+  {
+    id: "ansible",
+    title: "Ansible",
+    color: "#808080",
+    link: "https://ansible.com",
+  },
+  {
+    id: "websocket",
+    title: "WebSockets",
+    color: "#808080",
+    link: "https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API",
+  },
+  {
+    id: "java",
+    title: "Java",
+    color: "#F58219",
+    link: "https://www.java.com",
+  },
+  {
+    id: "greenfoot",
+    title: "Greenfoot",
+    color: "#808080",
+    link: "https://greenfoot.org/",
+  },
+  {
+    id: "flutter",
+    title: "Flutter",
+    color: "#0468d7",
+    link: "https://flutter.dev",
+  },
+  {
+    id: "mysql",
+    title: "MySQL",
+    color: "#F29111",
+    link: "https://www.mysql.com/",
+  },
+  {
+    id: "mapbox",
+    title: "MapBox",
+    color: "#4264FA",
+    link: "https://www.mapbox.com/",
+  },
+  {
+    id: "css",
+    title: "CSS",
+    color: "#1B73BA",
+    link: "https://wiki.selfhtml.org/wiki/CSS/",
+  },
+  {
+    id: "html",
+    title: "HTML",
+    color: "#E14A24",
+    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    id: "google-cloud",
+    title: "Google Cloud",
+    color: "#808080",
+    link: "https://cloud.google.com/",
+  },
+  {
+    id: "google-cloud-pubsub",
+    title: "Google Cloud Pub Sub",
+    color: "#4285F4",
+    link: "https://cloud.google.com/pubsub",
+  },
+  {
+    id: "google-cloud-compute-engine",
+    title: "Google Cloud Compute Engine",
+    color: "#4285F4",
+    link: "https://cloud.google.com/compute",
+  },
+  {
+    id: "firebase-in-app-messaging",
+    title: "Firebase In-App Messaging",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/in-app-messaging",
+  },
+  {
+    id: "firebase-performance",
+    title: "Firebase Performance",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/performance",
+  },
+  {
+    id: "firebase-storage",
+    title: "Firebase Storage",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/storage",
+  },
+  {
+    id: "firebase-functions",
+    title: "Firebase Functions",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/functions",
+  },
+  {
+    id: "firebase-authentication",
+    title: "Firebase Authentication",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/authentication",
+  },
+  {
+    id: "firebase-crashlytics",
+    title: "Firebase Crashlytics",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/crashlytics",
+  },
+  {
+    id: "firebase-firestore",
+    title: "Firebase Firestore",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/firestore",
+  },
+  {
+    id: "firebase-cloud-messaging",
+    title: "Firebase Cloud Messaging",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/cloud-messaging",
+  },
+  {
+    id: "firebase-app-check",
+    title: "Firebase App-Check",
+    color: "#F47F0A",
+    link: "https://firebase.google.com/products/app-check",
+  },
+  {
+    id: "cloudflare-pages",
+    title: "Cloudflare Pages",
+    color: "#EF780C",
+    link: "https://pages.dev",
+  },
+  {
+    id: "cloudflare-workers",
+    title: "Cloudflare Workers",
+    color: "#EF780C",
+    link: "https://workers.dev",
+  },
+  {
+    id: "esxi",
+    title: "ESXi",
+    color: "#F38B00",
+    link: "https://www.vmware.com/products/esxi-and-esx.html",
+  },
+  {
+    id: "cplusplus",
+    title: "C++",
+    color: "#004481",
+    link: "https://cplusplus.com",
+  },
+  {
+    id: "nx",
+    title: "Nx",
+    color: "#012F56",
+    link: "https://nx.dev",
+  },
+  {
+    id: "docker",
+    title: "Docker",
+    color: "#2496ED",
+    link: "https://www.docker.com",
+  },
+  {
+    id: "react",
+    title: "React",
+    color: "#60DAFA",
+    link: "https://reactjs.org/",
+  },
+  {
+    id: "rust",
+    title: "Rust",
+    color: "#EB4700",
+    link: "https://www.rust-lang.org/",
+  },
+  {
+    id: "go",
+    title: "Go",
+    color: "#00ACD9",
+    link: "https://golang.org/",
+  },
+  {
+    id: "nodejs",
+    title: "node.js",
+    color: "#3C873A",
+    link: "https://nodejs.org/",
+  },
+  {
+    id: "typescript",
+    title: "TypeScript",
+    color: "#3179C7",
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    id: "javascript",
+    title: "JavaScript",
+    color: "#F7E01D",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    id: "cockroachdb",
+    title: "CockroachDB",
+    color: "#348641",
+    link: "https://www.cockroachlabs.com",
+  },
+  {
+    id: "scylla",
+    title: "Scylla",
+    color: "#57D1E5",
+    link: "https://www.scylladb.com",
+  },
+  {
+    id: "istio",
+    title: "Istio",
+    color: "#466BB0",
+    link: "https://istio.io",
+  },
+  {
+    id: "protobuf",
+    title: "Protocol Buffer",
+    color: "#F2F3F5",
+    link: "https://developers.google.com/protocol-buffers",
+  },
+  {
+    id: "grpc",
+    title: "gRPC",
+    color: "#F2F3F5",
+    link: "https://developers.google.com/protocol-buffers",
+  },
+  {
+    id: "grpc",
+    title: "gRPC",
+    color: "#4264FA",
+    link: "https://grpc.io/",
+  },
+  {
+    id: "kubernetes",
+    title: "Kubernetes",
+    color: "#336CE6",
+    link: "https://kubernetes.io",
+  },
+  {
+    id: "dart",
+    title: "Dart",
+    color: "#03589c",
+    link: "https://dart.dev/",
+  },
+  {
+    id: "nextjs",
+    title: "Next.js",
+    color: "#0D1117",
+    link: "https://nextjs.org/",
   },
 ] as const satisfies ContentTechnology[];
 
