12e5a1f2b2a798c909b07a08d97f19528e911249
Jan Klinge <jan@klydra.dev>
2025-02-17 19:50:48 +0100

    add 404

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="h-full w-full flex flex-row justify-center items-center gap-8">
+      <h1 className="font-bold text-xl">404</h1>
+      <p>not found</p>
+    </div>
+  );
+}
